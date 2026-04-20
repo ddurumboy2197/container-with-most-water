34f9c9d30fd20bab67ee08210daa149d6f6d875e
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-20 15:48:16 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        let area = Math.min(height[left], height[right]) * (right - left);
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+}
+```
+
+```javascript
+// Test
+let height = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(height)); // 49
